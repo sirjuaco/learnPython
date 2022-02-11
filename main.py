f89e5edb6f0a0a8ecfd9feb4465131b64386b199
Juan O. Cordero <juanosvaldoc@gmail.com>
2022-02-11 12:56:13 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+calculation_to_units = 24
+name_of_unit = "hours"
+
+
+def days_to_units(num_of_days):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+
+
+input("Hey user, enter a number of days and I will convert it to hours!\n")
