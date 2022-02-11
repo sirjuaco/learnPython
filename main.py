766c6f3eb1febc992f93b4a8865912af5d29f6f3
Juan O. Cordero <juanosvaldoc@gmail.com>
2022-02-11 16:15:29 -0400

    refactoring code

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ name_of_unit = "hours"
 
 
 def days_to_units(num_of_days):
-    if num_of_days > 0:
         return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
-    elif num_of_days == 0:
-        return "you entered a zero, please enter a valid positive number!"
+
+
+def validate_and_execute():
+    if user_input.isdigit():
+        user_input_number = int(user_input)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a zero, please enter a valid positive number!")
     else:
-        return "you entered a negative value, so no conversion for you!"
+        print("your input is not a valid number. Don't ruin my program")
 
 
 user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
-user_input_number = int(user_input)
+validate_and_execute()
 
-calculated_value = days_to_units(user_input_number)
-print(calculated_value)
+
+
